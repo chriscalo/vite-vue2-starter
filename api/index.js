43081c0d4222efb7427adb4144ff1028179d5f7d
Chris Calo <chris.calo@gmail.com>
2022-02-20 23:59:16 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const api = express();
 
 api.use("/", (req, res, next) => {
-  const url = req.url;
   res.type("application/json");
   res.send(`Hello, World!`);
 });
