0d81197eec4af99496a11eae9e8472f5878108bf
Chris Calo <chris.calo@gmail.com>
2022-03-07 20:55:19 -0500

    make the registry port dynamic

--- server/start.js
+++ server/start.js
@@ -1,12 +1,14 @@
 const config = require("config");
-const { run, registry } = require("~/util");
+const { getPort, run, registry } = require("~/util");
 
 const PRODUCTION = process.env.NODE_ENV === "production";
-process.env.REGISTRY_PORT = config.get("registry.port");
 
 (async function main() {
   console.log("Starting application…");
-  registry.start(process.env.REGISTRY_PORT);
+  
+  const registryPort = await getPort(config.get("registry.port"));
+  registry.start(registryPort);
+  process.env.REGISTRY_PORT = registryPort;
   
   run("npm", "start --workspace=proxy");
   run("npm", "start --workspace=ui");
